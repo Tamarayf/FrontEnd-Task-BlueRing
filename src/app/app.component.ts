eec0b57c26881b8dd89dc8d507c307a0d781742c
TAMARA <119131120+Tamara493@users.noreply.github.com>
2024-05-23 23:13:25 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import {HeroesComponent} from "./heroes/heroes.component";
+import {RouterOutlet} from "@angular/router";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import {MessagesComponent} from "./messages/messages.component";
+import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
+import {AppRoutingModule} from "./app-routing.module";
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    // HeroDetailComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    // Any additional providers
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } // <-- Export the AppModule class
